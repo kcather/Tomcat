48f1677d22fde3e731582f27e8660f94e7fe97a2
root <root@debian-8.7-amd64>
2017-02-28 02:59:21 +0000

    Changed bin permissions

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,17 @@ user 'root'
 
 package 'openjdk-7-jdk'
 
+group 'chef' do
+  action :create
+end
+
+user 'chef' do
+  manage_home false
+  shell '/bin/nologin'
+  group 'chef'
+  home '/opt/tomcat'
+end
+
 directory '/opt/tomcat' do
   action :create
   recursive true
@@ -16,27 +27,30 @@ execute 'extract_tomcat' do
   cwd '/tmp'
 end
 
-group 'tomcat' do
-  action :create
-end
-
-user 'chef' do
-  manage_home false
-  shell '/bin/nologin'
-  group 'tomcat'
-  home '/opt/tomcat'
-end
-
 execute 'chgrp -R chef /opt/tomcat/conf'
 
 directory '/opt/tomcat/conf' do
-  group 'tomcat'
+  group 'chef'
   mode '0474'
 end
 
 execute 'chmod g+r conf/*' do
   cwd '/opt/tomcat'
 end
+
+###
+execute 'chgrp -R tomcat bin' do
+  cwd '/opt/tomcat'
+end
+
+execute 'chmod g+rwx bin' do 
+  cwd '/opt/tomcat'
+end 
+
+execute 'chmod g+r bin/*' do
+  cwd '/opt/tomcat'
+end
+###
 
 execute 'chown -R chef webapps/ work/ temp/ logs/ conf/' do
   cwd '/opt/tomcat'
